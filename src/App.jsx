ff4ad533559d95c280e62326b4b44227d3cae5d9
dipesh0283 <dipesbista16@gmail.com>
2022-09-20 12:23:40 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import Select from 'react-select';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
+class App extends React.Component {
+  state = {
+    selectedOption: null,
+  };
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption }, () =>
+      console.log(`Option selected:`, this.state.selectedOption)
+    );
+  };
+  render() {
+    const { selectedOption } = this.state;
+
+    return (
+      <Select
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={options}
+      />
+    );
+  }
+}
+
+export default App;
